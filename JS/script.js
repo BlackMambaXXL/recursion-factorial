7f0e9851e3847503ae9f27a30162b3f19fbca7de
Niyaz <khairutdinov96@gmail.com>
2023-02-19 21:36:35 +0300

    added check for negative and fractional number

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
 "use strict";
 
 function factorial(x) {
-    if(x === 1) {
-        return x;
-    } else if (x < 1) {
-        console.log("Ошибка. Число меньше 1.");
+    if(x === 1 || x < 1) {
+        return 1;
+    } else if (Number.isInteger(x) === false) {
+        return "Ошибка. Число не целое.";
     } else {
         return x * factorial(x - 1);
     }
 }
 
-console.log(factorial(10));
+console.log(factorial(-2));
